2d01e8928c2844227d257a611367a08895733a71
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2023-07-09 20:12:18 -0300

    Update script.js

--- JS(S)/script.js
+++ JS(S)/script.js
@@ -1,1 +1,8 @@
-// por enquanto nada...
+let linkGithub = document.querySelector('#span-github');
+let linkLinkedin = document.querySelector('#span-linkedin');
+let menuMobile = document.querySelector('menu-bar-mobile');
+let linkGithubMobile = document.querySelector('span-github-mobile');
+
+menuMobile.addEventListener('click', function() {
+    
+});
