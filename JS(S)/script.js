fadb31ec630b3dd0e0a7ba0cfb575e6e8cfa55f0
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2023-08-24 21:26:39 -0300

    Update script.js

--- JS(S)/script.js
+++ JS(S)/script.js
@@ -1,7 +1,5 @@
 let body = document.getElementById('body');
 
-let linkGithub = document.querySelector('#span-github');
-let linkLinkedin = document.querySelector('#span-linkedin');
 let menuMobile = document.querySelector('menu-bar-mobile');
 let linkGithubMobile = document.querySelector('span-github-mobile');
 
